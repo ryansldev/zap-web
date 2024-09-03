52cbe2164b950ea51dfa1af79c6aeeb168adfd42
ryansldev <ryanlima1309@gmail.com>
2024-09-03 14:04:04 -0300

    feat: add mask to not have spaces in username

--- actions/signup/schema.ts
+++ actions/signup/schema.ts
@@ -12,6 +12,8 @@ export const SignupBodySchema = z.object({
   }),
   username: z.string().min(2, {
     message: "O nome de usuário precisa ter pelo menos 2 caracteres"
+  }).refine((value) => !/\s/.test(value), {
+    message: "O nome de usuário não deve conter espaços.",
   }),
   password: z.string().min(8, {
     message: "A senha precisa ter pelo menos 8 caracteres"
