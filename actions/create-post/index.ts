15e92ee4055977718927e5dbafdb851369301101
ryansldev <ryanlima1309@gmail.com>
2024-09-03 14:18:47 -0300

    trying to fix already logged user error

--- actions/create-post/index.ts
+++ actions/create-post/index.ts
@@ -7,6 +7,7 @@ import { createSafeAction } from "@/lib/create-safe-action";
 import { CreatePostBodySchema } from "./schema";
 import { revalidatePath } from "next/cache";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 async function handler({ text, parentId }: InputType): Promise<ReturnType> {
   try {
@@ -24,6 +25,13 @@ async function handler({ text, parentId }: InputType): Promise<ReturnType> {
     return { data }
   } catch(e: unknown) {
     const error = e as AxiosError<{ message: string }>
+    if(error.response?.data.message === "Author not found") {
+      cookies().delete('access_token')
+      cookies().delete('username')
+
+      redirect('/login')
+    }
+
     return {
       error: error.response?.data.message ?? error.message,
     }
