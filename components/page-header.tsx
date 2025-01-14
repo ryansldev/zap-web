394468177d7371f2dfb8187c3a27e1aa76e533a2
ryansldev <ryanlima1309@gmail.com>
2025-01-14 10:25:52 -0300

    bettering page header styles

--- components/page-header.tsx
+++ components/page-header.tsx
@@ -32,47 +32,50 @@ export function PageHeader({
   }
 
   return (
-    <div className={cn(
-      "flex gap-4 w-full relative mb-2",
-      (position === "top" || position === "transparent-top") && "absolute w-full gap-8 top-0 left-0",
-      position === "transparent-top" && "bg-transparent",
-      position === "top" && "bg-[#0f0f0f]",
-      centered && "text-center",
-      size === "lg" && "p-8",
-      size === "md" && "p-6",
-      size === "sm" && "p-4"
-    )}>
-      <Button
-        variant={position === "transparent-top" ? "ghost" : (
-          position === "inline" ? "ghost" : "secondary"
-        )}
-        size="icon"
-        onClick={onBack}
-        className={cn(position !== "inline" && "absolute")}
-      >
-        <ArrowLeft className="h-6 w-6" />
-      </Button>
-      <div className="flex flex-col w-full">
-        <h1 className="text-2xl font-bold">
-          { title }
-          <Emoji
-            name="brazil"
-            alt="Brazil flag"
-            ext="svg"
-            height={24}
-            width={24}
-            className="ml-2"
-          />
-        </h1>
-        <span className={cn("opacity-70 font-regular", centered && "max-w-[250px] mx-auto truncate")}>
-          {description}
-        </span>
+    <>
+      <div className={cn(
+        "flex gap-4 w-full relative mb-2",
+        (position === "top" || position === "transparent-top") && "absolute w-full gap-8 top-0 left-0",
+        position === "transparent-top" && "bg-transparent",
+        position === "top" && "bg-[#0f0f0f]",
+        centered && "text-center",
+        size === "lg" && "p-8",
+        size === "md" && "p-6",
+        size === "sm" && "p-4"
+      )}>
+        <Button
+          variant={position === "transparent-top" ? "ghost" : (
+            position === "inline" ? "ghost" : "secondary"
+          )}
+          size="icon"
+          onClick={onBack}
+          className={cn("h-full absolute top-0 bottom-0", position !== "inline" && "absolute")}
+        >
+          <ArrowLeft className="h-6 w-6" />
+        </Button>
+        <div className="flex flex-col w-full">
+          <h1 className="text-2xl font-bold">
+            { title }
+            <Emoji
+              name="brazil"
+              alt="Brazil flag"
+              ext="svg"
+              height={24}
+              width={24}
+              className="ml-2"
+            />
+          </h1>
+          <span className={cn("opacity-70 font-regular", centered && "max-w-[250px] mx-auto truncate")}>
+            {description}
+          </span>
+        </div>
       </div>
+
       { position === "inline" && (
         <Separator
           className="absolute bottom-2 left-0"
         />
       )}
-    </div>
+    </>
   )
 }
